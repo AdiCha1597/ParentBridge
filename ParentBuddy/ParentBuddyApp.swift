f704107a5b5fe2029cbfc6ccce732e375ab9bed6
Aditya Chandras <adityachandras@Aditya-Mac.local>
2024-07-30 15:44:13 -0700

    Initial commit

---
+++ ParentBuddy/ParentBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParentBuddyApp.swift
+//  ParentBuddy
+//
+//  Created by Aditya Chandras on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ParentBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
